021e6dd8b4998e0bdda4e868ceca9a16ce3be811
Vojtech Jasny <vojtech.jasny@gooddata.com>
2012-09-30 12:15:35 +0200

    Refactored omnibox and added tests.

--- src/omnibox.js
+++ src/omnibox.js
@@ -1,17 +1,51 @@
-/**
- * When iframe is pasted to omnibox parse our its src attr, append export parameter
- * and navigate to the resulting uri.
- */
-function onOmniboxInputEntered(text) {
-    var matches = text.match(/src="(.*?)"/i);
+function Omnibox() {}
 
-    if (matches && matches[1]) {
-        var uri = matches[1] + '&export=1';
-        navigate(uri);
-    } else if (text.indexOf('/') === 0) {
-        navigate('https://localhost' + text);
+Omnibox.prototype = {
+
+    bind: function() {
+        var callback = this.onInputEntered.bind(this);
+        chrome.omnibox.onInputEntered.addListener(callback);
+    },
+
+    navigate: function(url) {
+        chrome.tabs.getSelected(null, function(tab) {
+            if (tab.url === 'chrome://newtab/') {
+                chrome.tabs.update(tab.id, { url: url });
+            } else {
+                chrome.tabs.create({ url: url });
+            }
+        });
+    },
+
+    getResultUrl: function(text) {
+        var url, matches = text.match(/src="(.*?)"/i);
+
+        if (text.indexOf('/') === 0) {
+            url = 'https://localhost' + text;
+        } else if (matches && matches[1]) {
+            if (text.indexOf('dashboard.html') !== -1) {
+                url = matches[1] + '&export=1';
+            } else {
+                url = matches[1];
+            }
+        }
+
+        return url;
+    },
+
+    /**
+     * When iframe is pasted to omnibox parse our its src attr, append export parameter
+     * and navigate to the resulting uri.
+     */
+    onInputEntered: function(text) {
+        var url = this.getResultUrl(text);
+        url && this.navigate(url);
     }
+};
 
+if (typeof module !== 'undefined') {
+    module.exports = Omnibox;
+} else {
+    var omnibox = new Omnibox();
+    omnibox.bind();
 }
-
-chrome.omnibox.onInputEntered.addListener(onOmniboxInputEntered);
---
+++ test/omnibox.js
@@ -0,0 +1,41 @@
+var assert = require('chai').assert;
+var sinon = require('sinon');
+
+var Omnibox = require('../src/omnibox.js');
+
+describe('omnibox redirection', function() {
+    var omnibox;
+
+    beforeEach(function() {
+        omnibox = new Omnibox();
+    });
+
+    it('should redirect relative uri to localhost', function() {
+        var input = '/gdc/projects/5wvh1suon0ix9xso2lfhjwq29j7nrezw',
+            expected = 'https://localhost/gdc/projects/5wvh1suon0ix9xso2lfhjwq29j7nrezw';
+
+        assert.equal(omnibox.getResultUrl(input), expected);
+    });
+
+    var embeddedDashboardUrl = '<iframe frameborder="0" src="https://secure.gooddata.com/dashboard.html#project=/gdc/projects/5wvh1suon0ix9xso2lfhjwq29j7nrezw&dashboard=/gdc/md/5wvh1suon0ix9xso2lfhjwq29j7nrezw/obj/42992" width="100%" height="390px"></iframe>';
+    var otherUrl = '<iframe frameborder="0" src="https://secure.gooddata.com/" width="100%" height="390px"></iframe>';
+
+    it('should redirect dashboard iframe to export uri', function() {
+        var expectedUrl = 'https://secure.gooddata.com/dashboard.html#project=/gdc/projects/5wvh1suon0ix9xso2lfhjwq29j7nrezw&dashboard=/gdc/md/5wvh1suon0ix9xso2lfhjwq29j7nrezw/obj/42992&export=1';
+        assert.equal(omnibox.getResultUrl(embeddedDashboardUrl), expectedUrl);
+    });
+
+    it('should redirect iframe to src uri', function() {
+        var expectedUrl = 'https://secure.gooddata.com/';
+        assert.equal(omnibox.getResultUrl(otherUrl), expectedUrl);
+    });
+
+    it('should obtain redirect uri and navigate to it', function() {
+        sinon.stub(omnibox, 'getResultUrl').returns('bar');
+        sinon.stub(omnibox, 'navigate');
+
+        omnibox.onInputEntered('foo');
+        assert.isTrue(omnibox.getResultUrl.calledWithExactly('foo'));
+        assert.isTrue(omnibox.navigate.calledWithExactly('bar'));
+    });
+});
