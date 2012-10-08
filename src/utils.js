4af2795d12e77fc4e4a093ac3c1768542192b63f
Vojtech Jasny <vojtech.jasny@gooddata.com>
2012-10-08 14:54:30 +0200

    Renamed all files.

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * Navigate to an uri in the currently selected tab.
- */
-function navigate(url) {
-    chrome.tabs.getSelected(null, function(tab) {
-        chrome.tabs.update(tab.id, { url: url });
-    });
-}
